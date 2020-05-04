f1b6a53601ca523438ec8caf60df914de14e16b3
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-05-04 14:59:11 +0300

    Create toolbar.py

---
+++ toolbar.py
@@ -0,0 +1,16 @@
+import time, sys
+
+def update_progress(progress):
+    barLength = 42
+    status = ""
+    if isinstance(progress, int):
+        progress = float(progress)
+    block = int(round(barLength*progress))
+    text = "\r {1}% {0} {2}".format( "#"*block + " " *(barLength-block), int(progress*100), status)
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
+def default():
+    for i in xrange(101):
+        update_progress(i/100.0)
+        time.sleep(0.1)
